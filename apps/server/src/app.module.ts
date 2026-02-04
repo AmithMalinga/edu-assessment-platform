1bf786596008f314e78c0893ca1ab2e2e6ebf97a
AmithMalinga <malingaamith1@gmail.com>
2026-02-04 17:56:25 +0530

    feat: Initialize new NestJS server application with local authentication, Swagger, and a temporary Express server for JWT auth.

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -5,10 +5,18 @@ import { PrismaService } from './prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { StudentsModule } from './students/students.module';
 import { TestimonialsModule } from './testimonials/testimonials.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), StudentsModule, TestimonialsModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        AuthModule,
+        StudentsModule,
+        TestimonialsModule
+    ],
     controllers: [AppController],
-    providers: [AppService, PrismaService],
+    providers: [AppService],
 })
 export class AppModule { }
