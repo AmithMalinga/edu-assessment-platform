149ecdd4126e5fc855735c57021b3f659a8ffb98
AmithMalinga <malingaamith1@gmail.com>
2026-02-21 20:24:34 +0530

    refactor: streamline PrismaService initialization and re-enable PrismaModule import in AppModule

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+// import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { ValidationPipe } from '@nestjs/common';
 
@@ -9,17 +9,17 @@ async function bootstrap() {
     app.enableCors();
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-    const config = new DocumentBuilder()
-        .setTitle('Edu Assessment Platform API')
-        .setDescription('The API description for the Education Assessment Platform')
-        .setVersion('1.0')
-        .addBearerAuth()
-        .build();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    // Swagger temporarily disabled due to type conflicts
+    // const config = new DocumentBuilder()
+    //     .setTitle('Edu Assessment Platform API')
+    //     .setDescription('The API description for the Education Assessment Platform')
+    //     .setVersion('1.0')
+    //     .addBearerAuth()
+    //     .build();
+    // const document = SwaggerModule.createDocument(app, config);
+    // SwaggerModule.setup('api', app, document);
 
     await app.listen(3001);
     console.log(`Server is running on: ${await app.getUrl()}`);
-    console.log(`Swagger is running on: ${await app.getUrl()}/api`);
 }
 bootstrap();
