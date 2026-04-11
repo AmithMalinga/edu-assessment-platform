de4fb85b4461a54c7ee02da9cca60cd1525b4407
Dhananjana Hirushan <135137760+hirushan2001@users.noreply.github.com>
2026-04-11 11:26:17 +0530

    feat: initialize admin index and web layout with Google Analytics integration

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
 
@@ -17,6 +18,22 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <Script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-3HVCM2SYG0"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                      window.dataLayer = window.dataLayer || [];
+                      function gtag(){dataLayer.push(arguments);}
+                      gtag('js', new Date());
+
+                      gtag('config', 'G-3HVCM2SYG0');
+                    `}
+                </Script>
+            </head>
             <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>{children}</body>
         </html>
     )
